14794e1809232d814071008a93ce678621f3ef4f
Bogdan Tkachenko <djedai@list.ru>
2023-02-10 19:41:29 +0300

    initial push

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LottieView
+        source={require('./assets/octopus-typing.json')}
+        autoPlay
+        loop
+      />
       <StatusBar style="auto" />
     </View>
   );
